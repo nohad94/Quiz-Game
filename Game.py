fe9672a6b5ddd46665d07b7b43fb4eff7437cba2
FakeScotch <8sohD$su!W53%9R>
2019-05-31 09:08:14 -0700

    added score tracker

--- Game.py
+++ Game.py
@@ -4,11 +4,13 @@ import random
 
 
 class Game:
+    NUM = 10
+    NUM_CORRECT = 0
+
     def __init__(self, master):
         self.user_answer = ""
         self.question_number = 0
-        self.number_of_correct_answers = 0
-        self.question_list = list(set([random.choice(list_of_questions) for x in range(10)]))
+        self.question_list = list(set([random.choice(list_of_questions) for x in range(Game.NUM)]))
 
         self.master = master
         self.master.geometry("500x700")
@@ -24,8 +26,8 @@ class Game:
         self.frame.columnconfigure(2, weight=1)
         self.frame.columnconfigure(4, weight=2)
 
-        self.question = random.choice(self.question_list)
-        self.title = Label(self.frame, width=35, height=10, text=self.question, font="times 19", wraplength=350)
+        self.question = self.question_list[0]
+        self.title = Label(self.frame, width=35, height=10, text="Start Game", font="times 19", wraplength=350)
         self.title.grid(row=0, column=0, columnspan=5, rowspan=2, sticky="nsew")
 
         self.button_label1 = Label(self.frame, width=20, height=4, font="times 5")
@@ -37,32 +39,30 @@ class Game:
         self.button_label4 = Label(self.frame, width=20, height=4, font="times 5")
         self.button_label4.grid(row=4, column=3)
 
+        self.score = Label(self.frame, text=(str(0) + " out of " + str(Game.NUM)))
+        self.score.grid(row=6, column=0, columnspan=5, rowspan=1)
+
         self.possibilities = dic_possibilities[self.question]
         self.possibility1 = self.possibilities[0]
         self.possibility2 = self.possibilities[1]
         self.possibility3 = self.possibilities[2]
         self.possibility4 = self.possibilities[3]
 
-        self.button1 = Button(self.button_label1, text=self.possibility1, width=20, height=4, command=self.return_text_1)
+        self.button1 = Button(self.button_label1, text="Yes", width=20, height=4, command=self.return_text_1)
         self.button1.grid()
 
-        self.button2 = Button(self.button_label2, text=self.possibility2, width=20, height=4, command=self.return_text_2)
+        self.button2 = Button(self.button_label2, text="No", width=20, height=4, command=self.return_text_2)
         self.button2.grid()
 
-        self.button3 = Button(self.button_label3, text=self.possibility3, width=20, height=4, command=self.return_text_3)
+        self.button3 = Button(self.button_label3, text="", width=20, height=4, command=self.return_text_3)
         self.button3.grid()
 
-        self.button4 = Button(self.button_label4, text=self.possibility4, width=20, height=4, command=self.return_text_4)
+        self.button4 = Button(self.button_label4, text="", width=20, height=4, command=self.return_text_4)
         self.button4.grid()
 
     def return_text_1(self):
         self.user_answer = self.button1["text"]
-        if self.user_answer == dic_answers[self.title["text"]]:
-            self.number_of_correct_answers += 1
-            print(self.question_list)
-            self.question_list = self.question_list[1:]
-            print(self.question_list)
-            #  reseting the text of button and question labels
+        if self.user_answer == "Yes":
             self.title["text"] = self.question_list[0]
             self.possibilities = dic_possibilities[self.title["text"]]
             self.possibility1 = self.possibilities[0]
@@ -74,10 +74,12 @@ class Game:
             self.possibility4 = self.possibilities[3]
             self.button4["text"] = self.possibility4
         else:
-            print(self.question_list)
+            if self.user_answer == dic_answers[self.title["text"]]:
+                Game.NUM_CORRECT += 1
+                self.score["text"] = (str(Game.NUM_CORRECT) + " out of " + str(Game.NUM))
+            else:
+                pass
             self.question_list = self.question_list[1:]
-            print(self.question_list)
-            #  reseting the text of button and question labels
             self.title["text"] = self.question_list[0]
             self.possibilities = dic_possibilities[self.title["text"]]
             self.possibility1 = self.possibilities[0]
@@ -91,27 +93,15 @@ class Game:
 
     def return_text_2(self):
         self.user_answer = self.button2["text"]
-        if self.user_answer == dic_answers[self.title["text"]]:
-            self.number_of_correct_answers += 1
-            print(self.question_list)
-            self.question_list = self.question_list[1:]
-            print(self.question_list)
-            #  reseting the text of button and question labels
-            self.title["text"] = self.question_list[0]
-            self.possibilities = dic_possibilities[self.title["text"]]
-            self.possibility1 = self.possibilities[0]
-            self.button1["text"] = self.possibility1
-            self.possibility2 = self.possibilities[1]
-            self.button2["text"] = self.possibility2
-            self.possibility3 = self.possibilities[2]
-            self.button3["text"] = self.possibility3
-            self.possibility4 = self.possibilities[3]
-            self.button4["text"] = self.possibility4
+        if self.user_answer == "No":
+            self.master.destroy()
         else:
-            print(self.question_list)
+            if self.user_answer == dic_answers[self.title["text"]]:
+                Game.NUM_CORRECT += 1
+                self.score["text"] = (str(Game.NUM_CORRECT) + " out of " + str(Game.NUM))
+            else:
+                pass
             self.question_list = self.question_list[1:]
-            print(self.question_list)
-            #  reseting the text of button and question labels
             self.title["text"] = self.question_list[0]
             self.possibilities = dic_possibilities[self.title["text"]]
             self.possibility1 = self.possibilities[0]
@@ -124,81 +114,40 @@ class Game:
             self.button4["text"] = self.possibility4
 
     def return_text_3(self):
-        self.user_answer = self.button2["text"]
+        self.user_answer = self.button3["text"]
         if self.user_answer == dic_answers[self.title["text"]]:
-            self.number_of_correct_answers += 1
-            print(self.question_list)
-            self.question_list = self.question_list[1:]
-            print(self.question_list)
-            #  reseting the text of button and question labels
-            self.title["text"] = self.question_list[0]
-            self.possibilities = dic_possibilities[self.title["text"]]
-            self.possibility1 = self.possibilities[0]
-            self.button1["text"] = self.possibility1
-            self.possibility2 = self.possibilities[1]
-            self.button2["text"] = self.possibility2
-            self.possibility3 = self.possibilities[2]
-            self.button3["text"] = self.possibility3
-            self.possibility4 = self.possibilities[3]
-            self.button4["text"] = self.possibility4
+            Game.NUM_CORRECT += 1
+            self.score["text"] = (str(Game.NUM_CORRECT) + " out of " + str(Game.NUM))
         else:
-            print(self.question_list)
-            self.question_list = self.question_list[1:]
-            print(self.question_list)
-            #  reseting the text of button and question labels
-            self.title["text"] = self.question_list[0]
-            self.possibilities = dic_possibilities[self.title["text"]]
-            self.possibility1 = self.possibilities[0]
-            self.button1["text"] = self.possibility1
-            self.possibility2 = self.possibilities[1]
-            self.button2["text"] = self.possibility2
-            self.possibility3 = self.possibilities[2]
-            self.button3["text"] = self.possibility3
-            self.possibility4 = self.possibilities[3]
-            self.button4["text"] = self.possibility4
+            pass
+        self.question_list = self.question_list[1:]
+        self.title["text"] = self.question_list[0]
+        self.possibilities = dic_possibilities[self.title["text"]]
+        self.possibility1 = self.possibilities[0]
+        self.button1["text"] = self.possibility1
+        self.possibility2 = self.possibilities[1]
+        self.button2["text"] = self.possibility2
+        self.possibility3 = self.possibilities[2]
+        self.button3["text"] = self.possibility3
+        self.possibility4 = self.possibilities[3]
+        self.button4["text"] = self.possibility4
 
     def return_text_4(self):
-        self.user_answer = self.button2["text"]
+        self.user_answer = self.button4["text"]
         if self.user_answer == dic_answers[self.title["text"]]:
-            self.number_of_correct_answers += 1
-            print(self.question_list)
-            self.question_list = self.question_list[1:]
-            print(self.question_list)
-            #  reseting the text of button and question labels
-            self.title["text"] = self.question_list[0]
-            self.possibilities = dic_possibilities[self.title["text"]]
-            self.possibility1 = self.possibilities[0]
-            self.button1["text"] = self.possibility1
-            self.possibility2 = self.possibilities[1]
-            self.button2["text"] = self.possibility2
-            self.possibility3 = self.possibilities[2]
-            self.button3["text"] = self.possibility3
-            self.possibility4 = self.possibilities[3]
-            self.button4["text"] = self.possibility4
+            Game.NUM_CORRECT += 1
+            self.score["text"] = (str(Game.NUM_CORRECT) + " out of " + str(Game.NUM))
         else:
-            print(self.question_list)
-            self.question_list = self.question_list[1:]
-            print(self.question_list)
-            #  reseting the text of button and question labels
-            self.title["text"] = self.question_list[0]
-            self.possibilities = dic_possibilities[self.title["text"]]
-            self.possibility1 = self.possibilities[0]
-            self.button1["text"] = self.possibility1
-            self.possibility2 = self.possibilities[1]
-            self.button2["text"] = self.possibility2
-            self.possibility3 = self.possibilities[2]
-            self.button3["text"] = self.possibility3
-            self.possibility4 = self.possibilities[3]
-            self.button4["text"] = self.possibility4
-
-    def new_question(self):
-        while self.question_number < 16:
-            self.question_number += 1
-            if self.question_number == 16:
-                break
-            if self.user_answer == dic_answers[self.title["text"]]:
-                self.number_of_correct_answers += 1
-                self.question_list = self.question_list[1:]
-            else:
-                self.question_list = self.question_list[1:]
+            pass
+        self.question_list = self.question_list[1:]
+        self.title["text"] = self.question_list[0]
+        self.possibilities = dic_possibilities[self.title["text"]]
+        self.possibility1 = self.possibilities[0]
+        self.button1["text"] = self.possibility1
+        self.possibility2 = self.possibilities[1]
+        self.button2["text"] = self.possibility2
+        self.possibility3 = self.possibilities[2]
+        self.button3["text"] = self.possibility3
+        self.possibility4 = self.possibilities[3]
+        self.button4["text"] = self.possibility4
 
--- Question.py
+++ Question.py
@@ -3,19 +3,25 @@ list_of_questions = ["What is the largest mammal in the world?",
                      "What is the most popular sport in the world?",
                      "What is the fifth element of the periodic table?",
                      "Which first electrical item did Thomas Edison invent?",
-                     "Which of these sports is always played on grass?"]
+                     "Which of these sports is always played on grass?",
+                     "What is the largest body in the solar system?",
+                     "What is the deepest ocean?",
+                     "What is the widest ocean?",
+                     "What is the largest country by land mass?",
+                     "What is the largest country by population?"]
 
 dic_possibilities = {
 
     "What is the largest mammal in the world?": ["Blue Whale", "Lion", "Elephant", "Giraffe"],
-
     "What is the most popular sport in the world?": ["Football", "Basketball", "Tennis", "American Football"],
-
     "What is the fifth element of the periodic table?": ["Hydrogen", "Oxygen", "Carbon", "Boron"],
-
     "Which first electrical item did Thomas Edison invent?": ["Telephone", "Printing Press", "Light Bulb", "Assembly Line"],
-
-    "Which of these sports is always played on grass?": ["Tennis", "Ice Hockey", "Basketball", "Football"]
+    "Which of these sports is always played on grass?": ["Tennis", "Ice Hockey", "Basketball", "Football"],
+    "What is the largest body in the solar system?": ["Earth", "The moon", "Jupiter", "The sun"],
+    "What is the deepest ocean?": ["Pacific Ocean", "Indian Ocean", "Atlantic Ocean", "Arctic Ocean"],
+    "What is the widest ocean?": ["Arctic Ocean", "Southern Ocean", "Atlantic Ocean", "Pacific Ocean"],
+    "What is the largest country by land mass?": ["China", "USA", "Russia", "Brazil"],
+    "What is the largest country by population?": ["India", "Russia", "China", "Egypt"]
 }
 
 
@@ -25,10 +31,12 @@ dic_answers = {
     "What is the most popular sport in the world?": "Football",
     "What is the fifth element of the periodic table": "Boron",
     "Which first electrical item did Thomas Edison invent?": "Light Bulb",
-    "Which of these sports is always played on grass?": "Football"
+    "Which of these sports is always played on grass?": "Football",
+    "What is the largest body in the solar system?": "The sun",
+    "What is the deepest ocean?": "Atlantic Ocean",
+    "What is the widest ocean?": "Pacific Ocean",
+    "What is the largest country by land mass?": "Russia",
+    "What is the largest country by population?": "China"
 }
-
-
-
 
 
